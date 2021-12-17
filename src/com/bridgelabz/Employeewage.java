e97eeb9a6022d3394ce141a1acb14f38e849f553
deepali <deepaalipatil@gmail.com>
2021-12-17 17:48:59 +0530

    UPDATED: Employeewage using switch case

--- src/com/bridgelabz/Employeewage.java
+++ src/com/bridgelabz/Employeewage.java
@@ -4,6 +4,8 @@ public class Employeewage {
 	public static void main(String[] args) {
  	   
  	   Employee employee1 = new Employee();
+ 	  
  	   Employee.employeecheck(employee1);
+ 	   
     } 
 }
