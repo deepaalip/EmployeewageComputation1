7dd24cc5cf6da98c960b944a4c282b58bcbba8bb
deepali <deepaalipatil@gmail.com>
2021-12-17 18:24:02 +0530

    ADDED: Employee attendance check

---
+++ src/com/bridgelabz/Employee.java
@@ -0,0 +1,22 @@
+package com.bridgelabz;
+
+public class Employee {
+	
+	
+	
+	public static void employeecheck( Employee check ) {  
+		final int IS_FULLTIME = 1;
+		
+		System.out.println("Welcome To Employee Wage Computation Program");
+		
+ 	   
+ 		 	 final int empcheck =((int) Math.floor(Math.random() * 10) % 2);
+ 		 	if ( empcheck == IS_FULLTIME )
+ 		       System.out.println("Employee is Present");
+ 		 else
+ 		       System.out.println("Employee is Abscent");
+ 		 }
+            
+	} 
+	  
+     
