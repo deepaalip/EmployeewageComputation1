7f3bf142aef1bfaba6f7c044571347ffcb9662bf
deepali <deepaalipatil@gmail.com>
2021-12-17 19:16:49 +0530

    ADDED: employee wage using switch case

--- src/com/bridgelabz/Employee.java
+++ src/com/bridgelabz/Employee.java
@@ -1,25 +1,33 @@
 package com.bridgelabz;
 
 public class Employee {
-	
-	
-	
 	public static void employeecheck( Employee check ) {  
 		final int IS_FULLTIME = 1;
-		
+		final int IS_PARTTIME = 2;
+		final int EMP_RATE_PER_HOUR =20;
+	     int emphrs = 0;
+	     int empwage = 0;
 		System.out.println("Welcome To Employee Wage Computation Program");
 		
  	   
- 		 	 final int empcheck =((int) Math.floor(Math.random() * 10) % 2);
- 		 	if ( empcheck == IS_FULLTIME ) 
- 		       System.out.println("Employee is Present");
- 		 	
- 		 	
- 		 else {
- 		       System.out.println("Employee is Abscent");
- 		 	}
-            
+ 		 	 final int empcheck =((int) Math.floor(Math.random() * 10)) % 3;
+ 		 	switch (empcheck) {
+            case IS_FULLTIME:
+               emphrs = 8;
+               System.out.println("Employee is present");
+               break;
+            case IS_PARTTIME:
+               emphrs = 4;
+               break;
+            default:
+               emphrs = 0;
+ 	         System.out.println("Employee is Abscent");
+           }
+ 		 	empwage = emphrs * EMP_RATE_PER_HOUR;
+ 	       System.out.println("Emp Wage: " + empwage);
 	} 
-	  
-     
 }
+	
+	
+	
+     
