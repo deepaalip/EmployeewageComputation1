0dd637b335aca9dc529af7e7790bf214f429eb4b
deepali <deepaalipatil@gmail.com>
2021-12-17 16:22:02 +0530

    ADDED: Employeewage computation using OOPS

---
+++ src/com/bridgelabz/Employeewage.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class Employeewage {
+	public static void main(String[] args) {
+ 	   
+ 	   Employee employee1 = new Employee();
+ 	   Employee.employeecheck(employee1);
+    } 
+}
