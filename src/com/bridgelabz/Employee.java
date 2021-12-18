57a46b90b000a5100373bc773250e970294d4586
deepali <deepaalipatil@gmail.com>
2021-12-18 14:31:32 +0530

    ADDED: compute empwage and save wage by company

--- src/com/bridgelabz/Employee.java
+++ src/com/bridgelabz/Employee.java
@@ -9,6 +9,7 @@ public class Employee {
 	private final int EMP_RATE_PER_HOUR;
 	private final int NO_OF_WORKING_DAYS ;
 	private final int max_hrs_per_month ;
+	private int totalempwage;
 	
 	public Employee (String company, int EMP_RATE_PER_HOUR, int NO_OF_WORKING_DAYS, int max_hrs_per_month){
 		
@@ -18,13 +19,13 @@ public class Employee {
 			this.max_hrs_per_month = max_hrs_per_month;
 		}
 		
-		private int computation_of_employee_wage(){
+		private void computation_of_employee_wage(){
 	     int emphrs = 0;
 	     int empwage = 0;
 	    int total_emp_hrs = 0;
 	    int num_of_days = 0;
 		System.out.println("Welcome To Employee Wage Computation Program");
-		int totalempwage=0;
+		
 		while (total_emp_hrs <= max_hrs_per_month && num_of_days < NO_OF_WORKING_DAYS ) {
 	         num_of_days++;
  	   
@@ -43,18 +44,22 @@ public class Employee {
            }
 		}
  		 	empwage = emphrs * EMP_RATE_PER_HOUR;
- 		     totalempwage += empwage;
- 		 	System.out.println("Total Emp Wage for the company : "+company+ " is " + totalempwage);
- 		 	return totalempwage;
+ 		    totalempwage += empwage;
+ 		 	
 	} 
+		@ Override
+		public String toString() {
+			return "Total Emp Wage for the company : "+company+ " is " + totalempwage;
+		}
 	  public static void main(String[] args) {
 		  Employee Dmart= new Employee ("Dmart",20,2,10);
 		  Employee Bigbasket= new Employee("Bigbasket",30,2,10);
-		  System.out.println("Total Emp Wage for the company : "+Dmart.company+ " is " + Dmart.computation_of_employee_wage());
-		  System.out.println("Total Emp Wage for the company : "+Bigbasket.company+ " is " + Bigbasket.computation_of_employee_wage());
-	  }
-  
+		 Dmart.computation_of_employee_wage();
+		 System.out.println(Dmart);
+         Bigbasket.computation_of_employee_wage();
+         System.out.println(Bigbasket);
 	
 }	
+}
 	
      
