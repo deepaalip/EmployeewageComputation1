7797469b417e99ec6347e1149728ca8629c7aeac
deepali <deepaalipatil@gmail.com>
2021-12-18 13:08:44 +0530

    ADDED: Refactor the code using class method

--- src/com/bridgelabz/Employee.java
+++ src/com/bridgelabz/Employee.java
@@ -1,12 +1,11 @@
 package com.bridgelabz;
 
 public class Employee {
-	public static void employeecheck( Employee check ) {  
-		final int IS_FULLTIME = 1;
-		final int IS_PARTTIME = 2;
-		final int EMP_RATE_PER_HOUR =20;
-		final int max_hrs_per_month = 100;
-		final int  NO_OF_WORKING_DAYS=20;
+	public static final int IS_FULLTIME = 1;
+	public static final int IS_PARTTIME = 2;
+	
+	public static int computation_of_employee_wage (String company, int EMP_RATE_PER_HOUR, int NO_OF_WORKING_DAYS, int max_hrs_per_month){
+		
 	     int emphrs = 0;
 	     int empwage = 0;
 	    int total_emp_hrs = 0;
@@ -29,12 +28,16 @@ public class Employee {
                emphrs = 0;
  	         System.out.println("Employee is Abscent");
            }
+		}
  		 	empwage = emphrs * EMP_RATE_PER_HOUR;
- 		 	totalempwage += empwage;
- 	       System.out.println("Emp Wage: " + empwage);
+ 		     totalempwage += empwage;
+ 		 	System.out.println("Total Emp Wage for the company : "+company+ " is " + totalempwage);
+ 		 	return totalempwage;
 	} 
-	    System.out.println("Total Emp Wage: " + totalempwage);
-  }
+	  public static void main(String[] args) {
+		 int totalwage = computation_of_employee_wage("Dmart",20,2,10);
+	  }
+  
 	
 }	
 	
